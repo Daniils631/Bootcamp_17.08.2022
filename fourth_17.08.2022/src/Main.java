ce3f39b9dd04065780cddb765cb67c97a75590a3
Daniils631 <111418512+Daniils631@users.noreply.github.com>
2022-08-17 16:03:32 +0300

    Add files via upload
    My fourth commit

---
+++ fourth_17.08.2022/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+        boolean bo = true;
+
+        Scanner scA = new Scanner(System.in);
+        System.out.println("Enter whole number: ");
+        int a = scA.nextInt();
+
+
+
+        if (a % 2 != 0){
+            System.out.println("number is odd " + bo);
+        }else{
+            System.out.println("number is even " + !bo);
+        }
+    }
+
+    // Solution
+    /*Enter a = 3;
+    output: number is odd true
+    Enter a = 4;
+    output: number is even false*/
+}
